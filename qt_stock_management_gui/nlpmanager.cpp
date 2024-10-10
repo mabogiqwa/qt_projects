bd3f1e6a8a155b7db4c8ed8fd9eed22d2aec0d91
Mabo Giqwa <122114072+numerous-kin0@users.noreply.github.com>
2024-10-10 13:20:50 +0200

    Update nlpmanager.cpp

--- qt_stock_management_gui/nlpmanager.cpp
+++ qt_stock_management_gui/nlpmanager.cpp
@@ -3,23 +3,19 @@
 NLPManager::NLPManager(QObject *parent)
     : QObject(parent), networkManager(new QNetworkAccessManager(this))
 {
-    // Initialize any necessary members here
+    
 }
 
 void NLPManager::analyzeText(const QString &text)
 {
-    // Prepare the network request (replace "YOUR_API_URL" with the actual API URL)
     QNetworkRequest request(QUrl("YOUR_API_URL"));
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
-
-    // Create the JSON object to send in the request
+    
     QJsonObject json;
-    json["text"] = text; // Assuming the API accepts a "text" field
+    json["text"] = text;
 
-    // Send the request (this assumes the API expects a POST request)
     QNetworkReply *reply = networkManager->post(request, QJsonDocument(json).toJson());
-
-    // Connect the reply finished signal to the appropriate slot
+    
     connect(reply, &QNetworkReply::finished, this, &NLPManager::onNetworkReplyFinished);
 }
 
@@ -31,13 +27,11 @@ void NLPManager::onNetworkReplyFinished()
         QJsonDocument jsonDoc = QJsonDocument::fromJson(response);
         QString analysisResult;
 
-        // Process the JSON response to extract the analysis result
         if (jsonDoc.isObject()) {
             QJsonObject jsonObject = jsonDoc.object();
-            analysisResult = jsonObject["result"].toString(); // Adjust this based on your API response
+            analysisResult = jsonObject["result"].toString(); 
         }
 
-        // Emit the signal with the result
         emit textAnalysisComplete(analysisResult);
 
         reply->deleteLater(); // Clean up the reply object
